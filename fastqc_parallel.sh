3f609750b6f475744d6cd78ad1e83e2b7c382c94
schosio <94955130+schosio@users.noreply.github.com>
2022-01-03 12:45:39 +0530

    Update fastqc_parallel.sh

--- fastqc_parallel.sh
+++ fastqc_parallel.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -v
 date
-find . -name "*.fastq" | parallel -j 30 -v -I% --max-args 1 fastqc 
+#will work for both fastq and fastq.gz files
+find . -type f -name "*.fastq" -o -name "*.fastq.gz" | parallel -j 10 -v -I% --max-args 1 fastqc 
+#combining all the fastqc html reports using multiqc
 multiqc .
